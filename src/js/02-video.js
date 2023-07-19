346e54a73d714e56af6f13b984b1175b8386261a
Volodymyr Ionaytis <blessfire91@gmail.com>
2023-07-19 12:26:43 +0200

    HW-done-pls -check

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+
+const STORAGE_KEY = 'videoplayer-current-time';
+const player = new Player('vimeo-player');
+
+onPageloading();
+player.on('timeupdate', throttle(onSaveTime, 1000));
+
+function onSaveTime({seconds}) {
+    const currentTime = seconds;
+    localStorage.setItem(STORAGE_KEY, currentTime);
+}
+
+function onPageloading() {
+    const saveCurrentTime = localStorage.getItem(STORAGE_KEY);
+    if (saveCurrentTime) {
+        player.setCurrentTime(saveCurrentTime);
+    }
+}
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,51 @@
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const formItems = {
+    form: document.querySelector('.feedback-form'),
+    input: document.querySelector('.feedback-form input'),
+    textarea: document.querySelector('.feedback-form textarea'),
+};
+
+formItems.form.addEventListener('submit', onFormSubmit);
+formItems.form.addEventListener('input', throttle(onFormInput, 500));
+
+let dataForm = {};
+
+function onFormInput(evt) {
+    const { name, value } = evt.target;
+    dataForm[name] = value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(dataForm))
+}
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    if (formItems.input.value.trim() === '' || formItems.textarea.value.trim() === '') {
+     return alert('Треба заповнити поля');
+    }
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    console.log(dataForm);
+    dataForm = {};
+}
+refillFields(); 
+function refillFields() {
+    const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+    if (savedData) {
+        const { email, message } = savedData;
+        if (email) {
+            formItems.input.value = email;
+            dataForm.email = email;
+        }
+        if (message) {
+            formItems.textarea.value = message;
+            dataForm.message = message
+        }
+    }
+}
+
+
+
 
