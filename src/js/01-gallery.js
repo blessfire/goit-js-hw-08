e86191cd3cc47f9f94aa7ceae19063d5e79633a2
Volodymyr Ionaytis <64978979+blessfire@users.noreply.github.com>
2023-07-28 15:45:47 +0200

    fix SimpleLightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,6 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-import Simplelightbox from 'simlelightbox';
+import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 // Change code below this line
