f59c13648e7df8d12aec8b7770032c702ce6cfe9
Volodymyr Ionaytis <64978979+blessfire@users.noreply.github.com>
2023-07-28 15:02:57 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,5 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-import simpleLightbox from 'simplelightbox';
-import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 // Change code below this line
